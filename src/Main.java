a6a2d1a72670c523091e299a0b1f5e8aa947618b
Ivan <ivan.olegovich603@gmail.com>
2024-03-14 13:24:32 +0300

    com15

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.List;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("From 15");
+        List<Integer> a;
+    }
+}
