934c5144906b0a37127b9ebf1e809d7f0df19e8b
Ivan <ivan.olegovich603@gmail.com>
2024-03-14 14:04:30 +0300

    com15666

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.util.List;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("From 15");
+        System.out.printf("From 15345");
         List<Integer> a;
     }
 }
